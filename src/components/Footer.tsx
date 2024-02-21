a1d78158d5cbd287328ffdf5fc94d5c2c8b44849
Abdullahi254 <abdush504@gmail.com>
2024-02-21 15:02:43 +0300

    created footer and header

---
+++ src/components/Footer.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import logo from "../../public/logo.png"
+import { usePathname } from 'next/navigation'
+
+type Props = {}
+
+const Footer = (props: Props) => {
+    const pathName = usePathname()
+
+    return (
+        <nav className={`w-full h-12 bg-gradient-to-r from-[#000000] to-[#1e1f1a] border-t-[1px] border-gray-800 px-8 py-2 z-[100]`}>
+            <div className='flex w-full justify-evenly items-center'>
+                <div className='cursor-pointer'>
+                    <Image src={logo} width={60} height={60} alt='sheng icon' priority />
+                </div>
+                <div className='space-x-8'>
+                    <Link href="/">
+                        <span className={pathName === '/' ? `text-gray-500 text-sm hover:text-gray-100 hover:underline underline`: 'text-gray-500 text-sm hover:text-gray-100 hover:underline'}>
+                            Home
+                        </span>
+                    </Link>
+                    <Link href="/characters">
+                        <span className={pathName === '/characters' ? `text-gray-500 text-sm hover:text-gray-100 hover:underline underline`: 'text-gray-500 text-sm hover:text-gray-100 hover:underline'}>
+                            Characters
+                        </span>
+                    </Link>
+                    <Link href="/episodes">
+                        <span className={pathName === '/episodes' ? `text-gray-500 text-sm hover:text-gray-100 hover:underline underline`: 'text-gray-500 text-sm hover:text-gray-100 hover:underline'}>
+                            Epidsodes
+                        </span>
+                    </Link>
+                    <Link href="https://github.com/Abdullahi254" target='_blank'>
+                        <span className={`text-gray-500 text-sm hover:text-gray-100 hover:underline`}>
+                            Github
+                        </span>
+                    </Link>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Footer
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,61 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import logo from "../../public/logo.png"
+import { usePathname } from 'next/navigation'
+
+type Props = {}
+
+const Navbar = (props: Props) => {
+
+    const [shadow, setShadow] = useState<boolean>(false)
+    const pathName = usePathname()
+
+    useEffect(() => {
+        const handleShadow = () => {
+            if (window.scrollY >= 90) {
+                setShadow(true)
+            } else {
+                setShadow(false)
+            }
+        }
+        window.addEventListener('scroll', handleShadow)
+    }, [])
+    return (
+        <nav className={shadow ? `shadow-xl bg-gradient-to-r from-[#1e1f1a] to-[#000000] fixed top-0 w-full min-h-[120] border-b-[1px] border-gray-700 px-8 z-[100]` : ` bg-black fixed top-0 w-full min-h-[120px] border-b-[1px] border-gray-800 px-8 z-[100]`}>
+            <div className=' flex flex-col space-y-4 justify-center h-full mx-auto max-w-7xl p-4'>
+
+
+                <div className='w-full flex justify-center pt-2'>
+                    <Link href="/">
+                        <Image src={logo} alt='R&M icon' height={80} priority className='cursor-pointer w-auto h-auto' />
+                    </Link>
+                </div>
+
+                <div className='w-full flex justify-center space-x-6'>
+                    <Link href="/">
+                        <span className={pathName === '/' ? `text-sm text-gray-100 underline` : `text-gray-100 text-sm hover:text-gray-300 hover:underline`}>
+                            Home
+                        </span>
+                    </Link>
+                    <Link href="/characters">
+                        <span className={pathName === `/characters` ? `text-sm text-gray-100 underline` : `text-gray-100 text-sm hover:text-gray-300 hover:underline`}>
+                            Characters
+                        </span>
+                    </Link>
+                    <Link href="/episodes">
+                        <span className={pathName === '/episodes' ? `text-sm text-gray-100 underline` : `text-gray-100 text-sm hover:text-gray-300 hover:underline`}>
+                            Episodes
+                        </span>
+                    </Link>
+                </div>
+
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
