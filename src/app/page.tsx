781231a7440ab40a48156125db6c137e637caca6
Abdullahi254 <abdush504@gmail.com>
2024-02-21 15:04:48 +0300

    finished project layout initialization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
-import Image from "next/image";
+import { LInfo, Location } from "@/types"
 
-export default function Home() {
+const getLocationList = async (page?:number):Promise<Location<LInfo>> => {
+  // function to get a list of locations from the rick and morty api
+  try {
+    const res = await fetch(`https://rickandmortyapi.com/api/location?page=${page}`)
+    return res.json()
+
+  } catch (er) {
+    throw new Error('Failed to fetch data')
+  }
+}
+
+export default async function Home() {
+  const locationNames = await getLocationList(2)
+  locationNames.results.forEach(res=>console.log(res.name))
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+    <main className="flex flex-col items-center min-h-screen py-20 px-6 max-w-7xl mx-auto">
+
     </main>
   );
 }
