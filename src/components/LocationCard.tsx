4bc8a2df40b8776a6e08b180f22af0a7fb273224
Abdullahi254 <abdush504@gmail.com>
2024-02-24 15:02:54 +0300

    arranged location card data in a user friendly manner

--- src/components/LocationCard.tsx
+++ src/components/LocationCard.tsx
@@ -11,23 +11,41 @@ const LocationCard = ({
   residentsData
 }: CombinedList) => {
   const [visibleCount, setVisibleCount] = useState<number>(5);
+  //when more button is clicked increase the visible count by an interval of five
   const handleMoreClick = () => {
     setVisibleCount(prevCount => prevCount + 5);
   };
+  //when less button is clicked reduce the visible count by an interval of five
+  const handleLessClick = () => {
+    setVisibleCount(prevCount => prevCount - 5);
+  }
   return (
-    <div className='bg-black border-2 border-gray-800 shadow-lg shadow-white flex flex-col space-y-2 p-4'>
-      <h2 className='text-white'>{location}</h2>
-      {residentsData.slice(0, visibleCount).map((res, index) =>
-        <div key={index} className='w-full flex flex-col justify-start'>
-          <Image src={res.image} alt={res.name} width={50} height={50}/>
-          <h2 className='text-white'>{res.name}</h2>
-          <h2 className='text-white'>{res.status}</h2>
-        </div>)}
-      {
-        residentsData.length > 5 && <button onClick={handleMoreClick} className='text-blue-300 underline text-end'>
-          More
-        </button>
+    <div className='bg-black border-2 border-gray-800 shadow-lg shadow-white flex flex-col space-y-2 py-4 px-4 w-full'>
+      <h1 className='text-base lg:text-lg text-blue-300 text-center tracking-wide'>{location}</h1>
+
+      {residentsData.length > 1 ? residentsData.slice(0, visibleCount).map((res, index) =>
+        <li key={index} className='w-full grid grid-cols-2 gap-8 items-center border-b-2 border-blue-300 py-4'>
+          <div className='flex items-center space-x-2'>
+            <Image src={res.image} alt={res.name} width={50} height={50} className='rounded-full' />
+            <h2 className='text-white text-xs lg:text-sm cursor-pointer hover:underline hover:text-gray-300'>{res.name}</h2>
+          </div>
+          <h2 className='text-white text-xs lg:text-sm mr-1'>Status: <span className={`${res.status === 'Dead' ? 'text-red-400' : res.status === 'Alive' ? 'text-green-400' : 'text-white'} `}>{res.status}</span></h2>
+        </li>) :
+        <h1 className='text-red-400 text-center text-xs'>NO RESIDENTS FOUND !</h1>
       }
+      <div className='flex justify-between pt-4'>
+        {
+          visibleCount >= 10 && <button onClick={handleLessClick} className='text-red-300 underline text-end text-xs lg:text-sm'>
+            Less
+          </button>
+        }
+        {
+          (residentsData.length > 5 && visibleCount < residentsData.length-1) && <button onClick={handleMoreClick} className='text-green-300 underline text-end text-xs lg:text-sm'>
+            More
+          </button>
+        }
+      </div>
+
 
     </div>
   )
--- src/components/LocationList.tsx
+++ src/components/LocationList.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 const LocationList = ({combinedList}: Props) => {
   return (
-    <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4 p-4 items-stretch'>
+    <div className='grid grid-cols-1 lg:grid-cols-3  gap-4 mt-4 p-4 items-start'>
        {
         combinedList.map((data, index)=><LocationCard key={index} location={data.location} residentsData={data.residentsData}/>)
        }
