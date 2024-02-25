6e2ab346050fe01e4a9040e9386cf4b62fd5019d
Abdullahi254 <abdush504@gmail.com>
2024-02-25 23:04:36 +0300

    added server side characters component to handle fetch character info from api

---
+++ src/app/characters/[id]/page.tsx
@@ -0,0 +1,38 @@
+import CharacterCard from '@/components/CharacterCard'
+import { CHInfo } from '@/types'
+import React from 'react'
+
+type Props = {
+    params: { id: string }
+}
+const getCharacterInfoById = async (id: string): Promise<CHInfo | undefined> => {
+    try {
+        const res = await fetch(`https://rickandmortyapi.com/api/character/${id}`)
+        const result = await res.json()
+        if (result.error) {
+            throw Error("ERROR FETCHING CHARACTER!")
+        }
+        return result
+    } catch (er) {
+        console.log(er)
+    }
+}
+const Characters = async ({ params }: Props) => {
+    const character = await getCharacterInfoById(params.id)
+    if (character) {
+        return (
+            <main className="flex flex-col items-center min-h-screen mt-[200px] p-6 max-w-7xl mx-auto">
+                <CharacterCard character={character} />
+            </main>
+        )
+    }
+    else {
+        return (
+            <main className="flex flex-col items-center min-h-screen mt-[200px] p-6 max-w-7xl mx-auto">
+                <div className='text-red-500 tracking-wide text-lg lg:text-xl'>COULD NOT FETCH CHARACTER!</div>
+            </main>
+        )
+    }
+}
+
+export default Characters
