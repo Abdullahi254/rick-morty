2492d72525274545f1d8de5c193579d4acd3e2b2
Abdullahi254 <abdush504@gmail.com>
2024-02-25 23:03:27 +0300

    coming soon episodes page

---
+++ src/app/episodes/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+function page({ }: Props) {
+    return (
+        <main className="flex flex-col items-center min-h-screen mt-[200px] p-6 max-w-7xl mx-auto">
+            <h1 className='text-white text-center text-lg'>COMING SOON!</h1>
+        </main>
+    )
+}
+
+export default page
